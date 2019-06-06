588a65eba4410eba94eb91836c7695506c6e5c71
omatamix <47761650+omatamix@users.noreply.github.com>
2019-06-06 13:46:28 -0400

    Some defs.

--- defs.h
+++ defs.h
@@ -5,3 +5,7 @@ typedef unsigned long long U160;
 
 #define NAME0 "Firestorm"
 #define BRD_SQ_NUM 252
+
+enum { EMPTY, rP, rN, rB, rR, rQ, rK, bP, bN, bB, bR, bQ, bK, yP, yN, yB, yR, yQ, yK, gP, gN, gB, gR, gQ, gK }
+
+#endif
