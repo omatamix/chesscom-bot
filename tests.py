d4fc673c92d3f668cad60292a67475e393a0c707
omatamix <47761650+omatamix@users.noreply.github.com>
2020-05-19 02:06:58 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,1 +1,9 @@
+import chesscom
 
+def tests():
+    assert sum([1, 2, 3]) == 6, "Should be 6"
+
+if __name__ == "__main__":
+    tests()
+    """ The tests have successfully been runned. """
+    print("Everything passed")
