f7dc4d2417db6ae44f95a905042a22049c420e38
omatamix <47761650+omatamix@users.noreply.github.com>
2020-08-07 17:28:53 -0400

    Enhance code.

--- chesscom.py
+++ chesscom.py
@@ -14,15 +14,11 @@ import backoff
 ENDPOINTS = {
     "arrow": "/bot?token={}&arrow={}",
     "chat": "/bot?token={}&chat={}",
-    "circle": "/bot?token={}&arrow={}",
-    "clear": "/bot?token={}&arrow=clear",
-    "play": "/bot?token={}&arrow{}",
-    "resign": "/bot?token={}&play=R",
+    "play": "/bot?token={}&play={}",
     "stream": "/bot?token={}&stream=1"
 }
 
 class ChessCom():
-    """ The 4pc ChessCom api handler. """
     def __init__(self, token, url, version):
         self.version = version
         self.token = token
@@ -31,6 +27,7 @@ class ChessCom():
         self.set_user_agent("?")
 
     def is_final(exception):
+	""" Determine if this request is final. """
         return isinstance(exception, HTTPError) and exception.response.status_code < 500
 
     @backoff.on_exception(backoff.constant,
@@ -39,51 +36,42 @@ class ChessCom():
         interval=0.1,
         giveup=is_final)
     def api_get(self, path):
+	""" Preform a get request. """
         url = urljoin(self.baseUrl, path)
         response = self.session.get(url, timeout=2)
         response.raise_for_status()
         return response.json()
 
-    # Keep incase chesscom adds a post route.
-    @backoff.on_exception(backoff.constant,
-        (RemoteDisconnected, ConnectionError, ProtocolError, HTTPError, ReadTimeout),
-        max_time=60,
-	interval=0.1,
-        giveup=is_final)
-    def api_post(self, path, data=None):
-        url = urljoin(self.baseUrl, path)
-        response = self.session.post(url, data=data, timeout=2)
-        response.raise_for_status()
-        return response.json()
-
     def clear(self):
-        url = urljoin(self.baseUrl, ENDPOINTS["clear"].format(self.token))
-        return requests.get(url, headers=self.header, stream=True)
+	""" Clears all of the arrows on the board. """
+        return self.api_get(ENDPOINTS["arrow"].format(self.token, "clear"))
 
-    def play(self, fromCoordinate, toCoordinate, promotionCode):
-        url = urljoin(self.baseUrl, ENDPOINTS["arrow"].format(self.token, fromCoordinate + toCoordinate + promotionCode))
-        return requests.get(url, headers=self.header, stream=True)
+    def play(self, move):
+        """ Preforms a move on the board. """
+        return self.api_get(ENDPOINTS["play"].format(self.token, move))
 
     def chat(self, text):
-        url = urljoin(self.baseUrl, ENDPOINTS["chat"].format(self.token, text))
-        return requests.get(url, headers=self.header, stream=True)
+	""" Sends a message to the chat. """
+        return self.api_get(ENDPOINTS["chat"].format(self.token, text))
 
-    def arrow(self, fromCoordinate, toCoordinate):
-        url = urljoin(self.baseUrl, ENDPOINTS["arrow"].format(self.token, fromCoordinate + toCoordinate))
-        return requests.get(url, headers=self.header, stream=True)
+    def arrow(self, arrow):
+	""" Sends an arrow to the board. """
+        return self.api_get(ENDPOINTS["arrow"].format(self.token, arrow))
 
-    def circle(self, coordinate):
-        url = urljoin(self.baseUrl, ENDPOINTS["circle"].format(self.token, coordinate + coordinate))
-        return requests.get(url, headers=self.header, stream=True)
+    def circle(self, move):
+	""" Sends a circle to the board. """
+        return self.api_get(ENDPOINTS["circle"].format(self.token, move + move))
 
     def get_game_stream(self):
+        """ Gets the games stream data. """
         url = urljoin(self.baseUrl, ENDPOINTS["stream"].format(self.token))
-        return requests.get(url, headers=self.header, stream=True)
+        return requests.get(url, headers = self.header, stream = True)
 
     def resign(self):
-        url = urljoin(self.baseUrl, ENDPOINTS["resign"].format(self.token))
-        return requests.get(url, headers=self.header, stream=True)
+	""" Resigns the current game. """
+        return self.api_get(ENDPOINTS["play"].format(self.token, "R"))
 
     def set_user_agent(self, username):
+	""" Sets the user agent. """
         self.header.update({"User-Agent": "chesscom-bot/{} user:{}".format(self.version, username)})
         self.session.headers.update(self.header)
